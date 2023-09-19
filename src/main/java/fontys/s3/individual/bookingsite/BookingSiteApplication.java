97bd2bf8aac9322bcbfba1c8eba7861e99cfeb6b
Thomas Vasanth <t.vasanththomas@student.fontys.nl>
2023-09-19 14:14:06 +0200

    gitlab

--- src/main/java/fontys/s3/individual/bookingsite/BookingSiteApplication.java
+++ src/main/java/fontys/s3/individual/bookingsite/BookingSiteApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingSiteApplication {
 
+    //test
+
     public static void main(String[] args) {
         SpringApplication.run(BookingSiteApplication.class, args);
     }
