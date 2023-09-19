e8cbe8cd32a62c747f8c6fda71a088dd894fb02b
Thomas Vasanth <t.vasanththomas@student.fontys.nl>
2023-09-19 14:13:19 +0200

    First

---
+++ src/main/java/fontys/s3/individual/bookingsite/BookingSiteApplication.java
@@ -0,0 +1,13 @@
+package fontys.s3.individual.bookingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fontys/s3/individual/bookingsite/BookingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package fontys.s3.individual.bookingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
