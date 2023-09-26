eb16ef83469b8f8f53c13bdc46cadce95456e5b7
Thomas Vasanth <t.vasanththomas@student.fontys.nl>
2023-09-26 14:36:34 +0200

    Tests for Create users imp

---
+++ src/test/java/fontys/s3/individual/bookingsite/business/UserUseCaseImp/CreateUserUseCaseImpTest.java
@@ -0,0 +1,79 @@
+package fontys.s3.individual.bookingsite.business.UserUseCaseImp;
+
+import fontys.s3.individual.bookingsite.business.exception.UserNameAlreadyExistsException;
+import fontys.s3.individual.bookingsite.domain.request.UserRequest.CreateUserRequest;
+import fontys.s3.individual.bookingsite.domain.response.UserResponse.CreateUserResponse;
+import fontys.s3.individual.bookingsite.persistence.UserRepository;
+import fontys.s3.individual.bookingsite.persistence.entity.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CreateUserUseCaseImpTest
+{
+    @Mock
+    private UserRepository userRepositoryMock;
+
+    @InjectMocks
+    private CreateUserUseCaseImp createUserUseCaseImp;
+
+    @Test
+    void createUser_ValidInput_SavesTheUser()
+    {
+        // arrange
+        CreateUserRequest request = CreateUserRequest.builder()
+                .username("Thomas")
+                .email("Thomas@gmail.com")
+                .password("Password")
+                .build();
+
+        UserEntity userEntity = UserEntity.builder()
+                .id(1L)
+                .build();
+
+        when(userRepositoryMock.existsByUserName(request.getUsername())).thenReturn(false);
+        ArgumentCaptor<UserEntity> userEntityCaptor = ArgumentCaptor.forClass(UserEntity.class);
+        when(userRepositoryMock.save(userEntityCaptor.capture())).thenReturn(userEntity);
+
+
+        // act
+        CreateUserResponse response = createUserUseCaseImp.createUser(request);
+
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(1L, response.getId());
+
+        // Verify that the correct UserEntity was saved
+        UserEntity capturedUserEntity = userEntityCaptor.getValue();
+        assertEquals(request.getUsername(), capturedUserEntity.getUsername());
+        assertEquals(request.getPassword(), capturedUserEntity.getPassword());
+        assertEquals(request.getEmail(), capturedUserEntity.getEmail());
+    }
+
+
+    @Test
+    void createUser_InvalidInput_ThrowsException()
+    {
+        // arrange
+        CreateUserRequest request = CreateUserRequest.builder()
+                .username("Thomas")
+                .email("Thomas@gmail.com")
+                .password("Password")
+                .build();
+
+        when(userRepositoryMock.existsByUserName(request.getUsername())).thenReturn(true);
+
+        //act and assert
+
+        assertThrows(UserNameAlreadyExistsException.class, () ->
+            createUserUseCaseImp.createUser(request));
+
+    }
+}
